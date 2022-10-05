6f8bf49bfb3ccf136def096d4469d0fd507bfe12
Jeanette Sonnerborg <jv_88_@hotmail.com>
2022-10-05 21:44:13 +0200

    Changes. + CategoryService

---
+++ Client/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using BlazingShop.Shared;
+
+namespace BlazingShop.Client.Services.CategoryService
+{
+    public interface ICategoryService
+    {
+        List<Category> Categories { get; set; }
+
+        void LoadCategories();
+    }
+}
